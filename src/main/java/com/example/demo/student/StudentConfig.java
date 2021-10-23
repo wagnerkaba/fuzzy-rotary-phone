b778ec3c879f5ae5fc9caa1ed3cf143c1204d4a9
wagnerkaba <bom.link+github@gmail.com>
2021-10-23 16:08:28 -0300

    Configuração do banco de dados postgree e implementação do método GET

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student maria = new Student(
+                    "Maria",
+                    "maria@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    21
+            );
+            Student jose = new Student(
+                    "Jose",
+                    "jose@gmail.com",
+                    LocalDate.of(2004, Month.JANUARY, 5),
+                    21
+            );
+
+            repository.saveAll(List.of(maria, jose));
+        };
+    }
+
+
+}
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,16 +10,16 @@ import java.util.List;
 @Service
 public class StudentService {
 
-    public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Miriam",
-                        "miriam@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21
-                )
+    private final StudentRepository studentRepository;
 
-        );
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
+    public List<Student> getStudents(){
+        return studentRepository.findAll();
+
     }
 }
