da98c0dd97eca3703d32b512fbedc3b90f956103
wagnerkaba <bom.link+github@gmail.com>
2021-10-23 16:43:03 -0300

    Implementação do método "POST" Alteração do application.properties para que apareça mensagem de erro

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -20,6 +21,16 @@ public class StudentService {
 
     public List<Student> getStudents(){
         return studentRepository.findAll();
+
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentByEmail.isPresent()){
+            throw new IllegalStateException("Email taken");
+        }
+        studentRepository.save(student);
+
 
     }
 }
