aef128b6a4ec80a8bae366edda45fa53de0c4d93
wagnerkaba <bom.link+github@gmail.com>
2021-10-31 18:33:36 -0300

    ReadMe.MD modificado.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,6 +14,23 @@ import java.util.List;
 // Implementação do código: Spring Boot Tutorial
 // https://www.youtube.com/watch?v=9SGDpanrc8U
 
+// OBS: Para configurar o banco de dados PostGres no Heroku é necessário fazer com que o Heroku
+// utilize as configurações no arquivo application-heroku.properties
+//
+// Dessa forma, o arquivo application. properties pode ser utilizado para confirar o banco de dado
+// local, enquanto o application-heroku.properties é utilizado para configurações do heroku
+//
+// Para que o Heroku utilize o application-heroku.properties é preciso configurar
+// o arquivo procfile (que é um arquivo de configuração do heroku: procure procfile na internet)
+//
+// No entanto, não sei configurar esse procfile e não estou com tempo agora para fazer isso.
+//
+// Por isso, eu coloquei as configurações do heroku no arquivo application.properties.
+//
+// Por isso, se for executar esta API localmente, é preciso alterar o arquivo application.properties para
+// configurar o postgres local
+//
+// Para ver o arquivo procfile configurado corretamente, veja o projeto: https://github.com/wagnerkaba/literate-journey
 
 
 
