6a13715bc4110efc1692efc112a83889a64116db
wagnerkaba <bom.link+github@gmail.com>
2021-12-15 16:31:26 -0300

    Criação de teste unitário para o método selectExistsEmail. Configuração do banco de dados H2 para fins de teste

--- src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -2,12 +2,15 @@ package com.example.demo.student;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@DataJpaTest //Esta anotação é necessária para utilizar o H2 database configurado para testes
 class StudentRepositoryTest {
 
     @Autowired
@@ -17,16 +20,19 @@ class StudentRepositoryTest {
     @Test
     void ItShouldCheckIfStudentEmailExists() {
         //given
+        String email = "carlos@gmail.com";
         Student student = new Student(
                 "Carlos",
-                "carlos@gmail.com",
+                email,
                 LocalDate.of(2000, Month.JANUARY, 5),
                 Gender.MALE
                 );
         underTest.save(student);
 
         //when
+        Boolean expectedEmail = underTest.selectExistsEmail(email);
 
         //then
+        assertThat(expectedEmail).isTrue();
     }
 }
